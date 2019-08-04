6b41dcccb85190b630036c5b77664646260a3c43
agsrc <akshay0712@gwu.edu>
2019-08-03 20:04:11 -0400

    started code by author angela

---
+++ index.js
@@ -0,0 +1,8 @@
+var buttons = document.querySelectorAll(".drum").length;
+
+for (let index = 0; index < buttons; index++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",()=>{
+        alert("I got clicked");
+    })
+    
+}
